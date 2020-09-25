0349e19fa58f2d51d2af28905b92566caae0b82b
Matthias Leitl <a.dead.trousers@gmail.com>
2020-09-25 17:47:13 +0200

    Add device files

---
+++ AndroidProducts.mk
@@ -0,0 +1,3 @@
+LOCAL_PATH := device/Unihertz/Atom_XL
+
+PRODUCT_MAKEFILES := $(LOCAL_PATH)/omni_Atom_XL.mk
---
+++ omni_Atom_XL.mk
@@ -0,0 +1,32 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Add this line if your device is 64-bit
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+# Otherwise, If you have 32-bit device, add the below line instead of above line
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk
+
+# Another common config inclusion
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+
+# If you are building from OmniROM's minimal source, Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+PRODUCT_COPY_FILES += device/Unihertz/Atom_XL/prebuilt/zImage:kernel
+# Fles under $(LOCAL_PATH)/recovery/root/ gets automatically copied into recovery
+# PRODUCT_COPY_FILES += $(LOCAL_PATH)/recovery/root/*:root/*
+
+PRODUCT_DEVICE := Atom_XL
+PRODUCT_NAME := omni_Atom_XL
+PRODUCT_BRAND := Unihertz
+PRODUCT_MODEL := Atom_XL
+PRODUCT_MANUFACTURER := Unihertz
+
+# Forcefully add mtp support (adb is already there)
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+
+# Add fingerprint from Stock ROM build.prop
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    # These lines are from my device. You MUST Replace yours.
+    BUILD_FINGERPRINT="Unihertz/Atom_XL_EEA/Atom_XL:10/QP1A.190711.020/root.20200312.183343:user/release-keys" \
+    PRIVATE_BUILD_DESC="Atom_XL_EEA-user 10 QP1A.190711.020 root.20200312.183343 release-keys"
