0490250faba8ec36a5c01863a1b4b5e6503fa09d
Matthias Leitl <a.dead.trousers@gmail.com>
2020-09-25 20:25:20 +0200

    Correcting some errors

--- omni_Atom_XL.mk
+++ omni_Atom_XL.mk
@@ -3,10 +3,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # Add this line if your device is 64-bit
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Otherwise, If you have 32-bit device, add the below line instead of above line
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
 
 # Another common config inclusion
-$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 
 # If you are building from OmniROM's minimal source, Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
@@ -27,6 +27,5 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 # Add fingerprint from Stock ROM build.prop
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    # These lines are from my device. You MUST Replace yours.
     BUILD_FINGERPRINT="Unihertz/Atom_XL_EEA/Atom_XL:10/QP1A.190711.020/root.20200312.183343:user/release-keys" \
     PRIVATE_BUILD_DESC="Atom_XL_EEA-user 10 QP1A.190711.020 root.20200312.183343 release-keys"
