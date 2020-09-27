a4935544e6c60cb9acfdb4e9e791798c8fff65f5
Matthias Leitl <a.dead.trousers@gmail.com>
2020-09-27 18:54:17 +0200

    Make 64bit (hopefully) buildable now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,7 @@
 LOCAL_PATH := device/Unihertz/Atom_XL
 
+ALLOW_MISSING_DEPENDENCIES := true
+
 TARGET_BOARD_PLATFORM := mt6771               # From ro.mediatek.platform, but lowercase value
 TARGET_NO_BOOTLOADER := true
 TARGET_BOOTLOADER_BOARD_NAME := g63v71c2k_dfl_seea # From ro.product.board
@@ -34,7 +36,7 @@ TW_THEME := portrait_hdpi                     # Set the exact theme you wanna us
 DEVICE_RESOLUTION := 640x1136                 # The Resolution of your Device
 TARGET_SCREEN_HEIGHT := 1136                    # The height
 TARGET_SCREEN_WIDTH := 640                      # The width
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
+TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 # Set the Brightness Control File Path below (as per your chip/device)
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_SECONDARY_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
@@ -52,7 +54,6 @@ TW_DEFAULT_LANGUAGE := en                     # Set Default Language
 TW_EXTRA_LANGUAGES := false
 
 # Kernel
-TARGET_IS_64_BIT := true                      # true/false: Determine if the device is 64-bit or not
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 buildvariant=user androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_PAGESIZE := 2048
@@ -84,8 +85,6 @@ TARGET_BOARD_SUFFIX := _64                    # Remove if the device is 32-bit
 TARGET_USES_64_BIT_BINDER := true             # Remove if the device is 32-bit
 
 # Architecture
-# According to the device's architecture (64-bit or 32-bit)
-ifeq ($(TARGET_IS_64_BIT),true)
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
@@ -99,11 +98,3 @@ TARGET_2ND_CPU_VARIANT := generic
 TARGET_CPU_ABI_LIST_64_BIT := $(TARGET_CPU_ABI)
 TARGET_CPU_ABI_LIST_32_BIT := $(TARGET_2ND_CPU_ABI),$(TARGET_2ND_CPU_ABI2)
 TARGET_CPU_ABI_LIST := $(TARGET_CPU_ABI_LIST_64_BIT),$(TARGET_CPU_ABI_LIST_32_BIT)
-else
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := generic
-TARGET_CPU_ABI_LIST := $(TARGET_CPU_ABI),$(TARGET_CPU_ABI2)
-endif
--- omni_Atom_XL.mk
+++ omni_Atom_XL.mk
@@ -1,9 +1,6 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-
-# Add this line if your device is 64-bit
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-# Otherwise, If you have 32-bit device, add the below line instead of above line
-# $(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Another common config inclusion
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
